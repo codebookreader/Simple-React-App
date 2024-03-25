4be999d3de10a9eb178fa8ad21604a11f59ff678
Patrick Muiruri <patrick.muiruri@strathmore.edu>
2024-03-25 17:07:27 +0300

    Count Value

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const storageKeyName = "count";
+  const retrieveCountValue = () => Number(localStorage.getItem(storageKeyName));
+
+  const [count, setCount] = useState(retrieveCountValue);
+
+  const addCount = (count) => setCount(Number(count) + 1);
+
+  useEffect( () => {
+    localStorage.getItem(count);
+  }, [count]);
   return (
     
       <div classname="App">
